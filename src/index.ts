113a8efe643d1b9a814fc89a192e95d202e05476
Benjamin Reed <github@raccoonfink.com>
2020-01-24 14:08:37 -0500

    rearrange registration a bit

--- src/index.ts
+++ src/index.ts
@@ -5,20 +5,24 @@ import DateTimeFormatter from './formats/DateTimeFormatter';
 
 declare const moment: any;
 
-const register = (moment: any): void => {
+const register = (moment: any, fatal = true): void => {
   if (moment.tz) {
     console.log('Moment.js with timezone support detected; attaching Java format methods.');
     moment.fn.formatJavaSDF = new SimpleDateFormat();
     moment.fn.formatJavaDFT = new DateTimeFormatter();
   } else {
-    throw new Error('No timezone support detected on the provided moment object.');
+    console.error('Unable to attach Java format methods.  Moment.js object was invalid or missing timezone support.');
+    if (fatal) {
+      throw new Error('Moment.js object was invalid or missing timezone support.');
+    }
   }
 };
 
-if (moment && moment.tz) {
-  console.log('Moment.js with timezone support detected; attaching Java format methods.');
-  moment.fn.formatJavaSDF = new SimpleDateFormat();
-  moment.fn.formatJavaDFT = new DateTimeFormatter();
+/* Attempt to register with global Moment.js object if it's found. */
+// @ts-ignore
+if (window.moment) {
+  // @ts-ignore
+  register(window.moment, false);
 } else {
   console.warn('Moment.js with timezone support missing; skipping automatic moment-javaformat loading.');
   console.warn('Use momentJavaformat.register(moment) instead.');
