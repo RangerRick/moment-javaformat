149abc2e42dfa4435d8cea7916bd8bea73537127
Benjamin Reed <github@raccoonfink.com>
2020-01-25 13:28:44 -0500

    actually working registration

--- src/index.ts
+++ src/index.ts
@@ -8,17 +8,26 @@ declare const define: any;
 declare const require: any;
 //declare const moment: any;
 
-const register = (moment: any, fatal = true): void => {
+const register = (moment: any, fatal = true): boolean => {
   if (moment.tz) {
     console.log('Moment.js with timezone support detected; attaching Java format methods.');
-    moment.fn.formatJavaSDF = new SimpleDateFormat();
-    moment.fn.formatJavaDFT = new DateTimeFormatter();
+    const sdf = new SimpleDateFormat();
+    const dtf = new DateTimeFormatter();
+
+    moment.fn.formatJavaSDF = function(formatString: string): string {
+      return sdf.format(this, formatString);
+    };
+    moment.fn.formatJavaDTF = function(formatString: string): string {
+      return dtf.format(this, formatString);
+    };
+    return true;
   } else {
     console.error('Unable to attach Java format methods.  Moment.js object was invalid or missing timezone support.');
     if (fatal) {
       throw new Error('Moment.js object was invalid or missing timezone support.');
     }
   }
+  return false;
 };
 
 /* Attempt to register with global Moment.js object if it's found. */
@@ -26,26 +35,29 @@ const register = (moment: any, fatal = true): void => {
 // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
 (function (root: any, factory: any) {
   if (typeof define === 'function' && define.amd) {
-      // AMD. Register as an anonymous module.
-      define(['moment'], factory);
+    // AMD. Register as an anonymous module.
+    define(['moment-timezone'], factory);
   } else if (typeof exports === 'object') {
-      // Node. Does not work with strict CommonJS, but only CommonJS-like
-      // enviroments that support module.exports, like Node.
-      try {
-          module.exports = factory(require('moment'));
-      } catch (e) {
-          // If moment is not available, leave the setup up to the user.
-          // Like when using moment-timezone or similar moment-based package.
-          module.exports = factory;
-      }
+    // Node. Does not work with strict CommonJS, but only CommonJS-like
+    // enviroments that support module.exports, like Node.
+    try {
+      module.exports = factory(require('moment-timezone'));
+    } catch (e) {
+      // If moment is not available, leave the setup up to the user.
+      // Like when using moment-timezone or similar moment-based package.
+      module.exports = factory;
+    }
   }
 
   if (root) {
-      // Globals
-      root.registerJavaFormats = root.moment ? factory(root.moment) : factory;
+    // Globals
+    root.registerJavaFormats = root.moment ? factory(root.moment) : factory;
   }
 })(this, function (_moment: any) {
   register(_moment, false);
+  if (register) {
+    return _moment;
+  }
   return register;
 });
 
