b0d3c7cb2924c0c535ca1857b3ad9bb41f886dc0
Benjamin Reed <github@raccoonfink.com>
2020-01-25 13:10:17 -0500

    reintroduce autoloader

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
 import SimpleDateFormat from './formats/SimpleDateFormat';
 import DateTimeFormatter from './formats/DateTimeFormatter';
 
-declare const moment: any;
+declare const define: any;
+declare const require: any;
+//declare const moment: any;
 
 const register = (moment: any, fatal = true): void => {
   if (moment.tz) {
@@ -19,14 +22,32 @@ const register = (moment: any, fatal = true): void => {
 };
 
 /* Attempt to register with global Moment.js object if it's found. */
-// @ts-ignore
-if (window.moment) {
-  // @ts-ignore
-  register(window.moment, false);
-} else {
-  console.warn('Moment.js with timezone support missing; skipping automatic moment-javaformat loading.');
-  console.warn('Use momentJavaformat.register(moment) instead.');
-}
+
+// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+(function (root: any, factory: any) {
+  if (typeof define === 'function' && define.amd) {
+      // AMD. Register as an anonymous module.
+      define(['moment'], factory);
+  } else if (typeof exports === 'object') {
+      // Node. Does not work with strict CommonJS, but only CommonJS-like
+      // enviroments that support module.exports, like Node.
+      try {
+          module.exports = factory(require('moment'));
+      } catch (e) {
+          // If moment is not available, leave the setup up to the user.
+          // Like when using moment-timezone or similar moment-based package.
+          module.exports = factory;
+      }
+  }
+
+  if (root) {
+      // Globals
+      root.registerJavaFormats = root.moment ? factory(root.moment) : factory;
+  }
+})(this, function (_moment: any) {
+  register(_moment, false);
+  return register;
+});
 
 export default {
   register: register,
