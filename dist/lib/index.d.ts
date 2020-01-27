69d2dc475218be2e3c5195952977fe90949ee8f6
CircleCI Automated Commit <github@raccoonfink.com>
2020-01-27 12:54:05 -0500

    automated generation of dist/

--- dist/lib/index.d.ts
+++ dist/lib/index.d.ts
@@ -1,8 +1,6 @@
 import SimpleDateFormat from './formats/SimpleDateFormat';
 import DateTimeFormatter from './formats/DateTimeFormatter';
-declare const _default: {
-    register: (moment: any, fatal?: boolean) => boolean;
-};
-export default _default;
+declare const register: (moment: any, fatal?: boolean) => boolean;
+export default register;
 export { SimpleDateFormat, DateTimeFormatter };
 //# sourceMappingURL=index.d.ts.map
