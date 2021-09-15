d4794dea7d5eea511076107eddc279e8217f6305
Benjamin Reed <github@raccoonfink.com>
2021-09-15 16:30:31 -0400

    fix: rework how assets are generated for node vs. browser
    Bumping to 2.0.0 since it changes the locations of assets.

--- webpack.config.js
+++ webpack.config.js
@@ -2,23 +2,22 @@
 /* eslint-disable no-undef */
 const path = require('path');
 const process = require('process');
+const nodeExternals = require('webpack-node-externals');
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 const isProduction = (process.argv.filter((arg) => arg.match(/production/)).length > 0);
+const suffix = isProduction? '.min.js' : '.js';
 
-module.exports = {
+const generalConfig = {
   entry: {
     'moment-javaformat': path.resolve('./src/index'),
   },
   output: {
     path: path.resolve('./dist'),
-    filename: '[name]' + (isProduction? '.min' : '') + '.js',
     libraryTarget: 'umd',
-    umdNamedDefine: true,
-    chunkLoading: false,
-    wasmLoading: false
+    libraryExport: 'default',
   },
   externals: [
     'moment',
@@ -44,6 +43,36 @@ module.exports = {
   ]
 };
 
-if (!isProduction) {
-  // module.exports.plugins.push(new BundleAnalyzerPlugin());
+if (isProduction) {
+  generalConfig.devtool = 'cheap-module-source-map';
+} else {
+  // generalConfig.plugins.push(new BundleAnalyzerPlugin());
 }
+
+const browserConfig = {
+  target: 'web',
+  output: {
+    filename: `[name]${suffix}`,
+    path: path.resolve('./dist'),
+    libraryTarget: 'umd',
+    libraryExport: 'default',
+    globalObject: 'this',
+    umdNamedDefine: true,
+  },
+};
+
+const nodeConfig = {
+  target: 'node',
+  externals: [nodeExternals()],
+  output: {
+    filename: `node${suffix}`,
+    path: path.resolve('./dist'),
+    libraryTarget: 'umd',
+    libraryExport: 'default',
+  },
+};
+
+module.exports = [
+  Object.assign({}, generalConfig, browserConfig),
+  Object.assign({}, generalConfig, nodeConfig),
+];
