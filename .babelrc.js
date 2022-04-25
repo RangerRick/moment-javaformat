cc390855ae14060077ff841e0c19a51e02b00f28
Benjamin Reed <git@raccoonfink.com>
2022-04-25 13:02:20 -0400

    build(deps-dev): upgrade jest and babel-jest (and handle breaking changes)

--- .babelrc.js
+++ .babelrc.js
@@ -2,19 +2,9 @@ module.exports = {
   "presets": [
     ["@babel/preset-env", {
       "modules": "auto",
-      /*
-      "useBuiltIns": "usage",
-      "corejs": 3,
-      */
     },],
     '@babel/preset-typescript',
   ],
   "plugins": [
-    "@babel/plugin-proposal-async-generator-functions",
-    "@babel/plugin-proposal-class-properties",
-    "@babel/plugin-proposal-object-rest-spread",
-    "@babel/plugin-transform-destructuring",
-    "@babel/plugin-transform-for-of",
-    "@babel/plugin-transform-modules-commonjs",
   ]
 };
